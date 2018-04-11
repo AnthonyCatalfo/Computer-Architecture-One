5b75392c020ae7b9993b162d48ecaa2543da56c7
AnthonyCatalfo <anthonycatalfo@gmail.com>
2018-04-11 17:46:27 -0400

    Remove Hard Coding From loadMemory
    - In ls8.js:
    - Make load memory dynamic to third argument from Node command

--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -10,33 +10,24 @@ function loadMemory() {
 
     // Hardcoded program to print the number 8 on the console
 
-    const program = [ // print8.ls8
-        // "10011001", // LDI R0,8  Store 8 into R0
-        // "00000000",
-        // "00001000",
-        // "01000011", // PRN R0    Print the value in R0
-        // "00000000",
-        // "00000001"  // HLT       Halt and quit
-        "10011001", // # LDI R0,8  Load R0 with value 8
-        "00000000",
-        "00001000",
-        "10011001", // # LDI R1,9  Load R1 with value 9
-        "00000001",
-        "00001001",
-        "10101010", // # MUL R0,R1 Multiply R0*R1, storing result in R0
-        "00000000",
-        "00000001",
-        "01000011", // # PRN R0    Print value in R0
-        "00000000",
-        "00000001" // # HLT       Halt
-    ];
-
+    const data = process.argv[2];
+    const fs = require('fs');
+    const program = fs  
+        .readFileSync(data, 'utf8')
+        .split('\n')
+        .reduce((mem, binary) => {
+            if (binary[0] !== '#' && binary !== '') {
+                return mem.concat([binary.slice(0, 8)]);
+            } else {
+                return mem;
+            }
+        }, []);
     // Load the program into the CPU's memory a byte at a time
     for (let i = 0; i < program.length; i++) {
         cpu.poke(i, parseInt(program[i], 2));
     }
-}
-
+} 
+ 
 /**
  * Main
  */
