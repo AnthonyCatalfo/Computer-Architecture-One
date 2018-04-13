44a1826d9ac61e5664376e89332466878f95b8db
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-04-13 00:26:10 -0500

    Format Spacing/Prettier Format
    - in cpu, ls8, and ram js files:
    - format to 2 space setting
    - run "prettier" formatter

--- project/ls8/ram.js
+++ project/ls8/ram.js
@@ -1,16 +1,16 @@
 class RAM {
-    constructor(size) {
-        this.mem = new Array(size);
-        this.mem.fill(0);
-    }
+  constructor(size) {
+    this.mem = new Array(size);
+    this.mem.fill(0);
+  }
 
-    write(MAR, MDR) {
-        this.mem[MAR]=MDR;
-    }
+  write(MAR, MDR) {
+    this.mem[MAR] = MDR;
+  }
 
-    read(MAR) {
-        return this.mem[MAR];
-    }
+  read(MAR) {
+    return this.mem[MAR];
+  }
 }
 
 module.exports = RAM;
