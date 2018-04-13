e5ea8936cc6f9af5bdff11f0e52dc34b1a60b5cc
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-04-13 00:22:49 -0500

    Reorganize CPU Code/Prep For Refactor
    - in cpu.js:
    - alphabatize consts and switch cases
    - arrange var subsets logically

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -43,26 +43,32 @@ class CPU {
     }
 
     tick() {
-        const IR = this.reg.PC;
+        const ADD = 0b10101000;
+        const CALL = 0b01001000;
+        const HLT = 0b00000001;
         const LDI = 0b10011001;
         const MUL = 0b10101010;
-        const ADD = 0b10101000;
-        const PRN = 0b01000011;
-        const HLT = 0b00000001;
-        const PUSH = 0b01001101;
         const POP = 0b01001100;
-        const CALL = 0b01001000;
+        const PRN = 0b01000011;
+        const PUSH = 0b01001101;
         const RET = 0b00001001;
-
+        
+        const IR = this.reg.PC;
         let operandA = this.ram.read(IR + 1);
         let operandB = this.ram.read(IR + 2);
 
         switch (this.ram.read(IR)) {
+            case ADD:
+                this.alu('ADD', operandA, operandB);
+                break;
+            case CALL:
+                this.reg[SP] -= 1;
+                this.ram.write(this.reg[SP], this.reg.PC + 1);
+                this.reg.PC = this.reg[1];
+                subroutine = true;
+                break;
             case HLT:
                 this.stopClock();
-                break;
-            case PRN:
-                console.log(this.reg[operandA]);
                 break;
             case LDI:
                 this.reg[operandA] = operandB;
@@ -70,22 +76,16 @@ class CPU {
             case MUL:
                 this.alu('MUL', operandA, operandB);
                 break;
-            case ADD:
-                this.alu('ADD', operandA, operandB);
-                break;
-            case PUSH:
-                this.reg[SP] -= 1;
-                this.ram.write(this.reg[SP], this.reg[operandA]);
-                break;
             case POP:
                 this.reg[operandA] = this.ram.read(this.reg[SP]);
                 this.reg[SP] += 1;
                 break;
-            case CALL:
+            case PRN:
+                console.log(this.reg[operandA]);
+                break;
+            case PUSH:
                 this.reg[SP] -= 1;
-                this.ram.write(this.reg[SP], this.reg.PC + 1);
-                this.reg.PC = this.reg[1];
-                subroutine = true;
+                this.ram.write(this.reg[SP], this.reg[operandA]);
                 break;
             case RET:
                 this.reg.PC = this.ram.read(this.reg[SP]);
